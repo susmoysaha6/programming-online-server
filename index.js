d8655cdc723d3a0a1b49c47729ac5bf50e8a21cb
susmoysaha6 <susmoysaha6@gmail.com>
2022-10-26 14:42:34 +0600

    api created for single course

--- index.js
+++ index.js
@@ -8,12 +8,19 @@ const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Course API Running')
-})
+});
 
 app.get('/courses', (req, res) => {
     res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course._id === id);
+    res.send(selectedCourse);
 })
+
 
 app.listen(port, () => {
     console.log('e learning server running on port', port);
-})
+});
