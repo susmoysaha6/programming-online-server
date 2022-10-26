a0ea638bbc34cc0e2897205f2325d350d149ea7f
susmoysaha6 <susmoysaha6@gmail.com>
2022-10-26 14:37:12 +0600

    api created for all courses

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Course API Running')
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
 })
 
 app.listen(port, () => {
